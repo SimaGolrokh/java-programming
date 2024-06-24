afa2d33967367e0b2c7326c78c994b25a27d64af
sima golrokh bahoush <sima.golrokh@gmail.com>
2024-06-24 12:19:13 +0200

    basic

---
+++ linkdinlearning/src/Types.java
@@ -0,0 +1,17 @@
+public class Types {
+    public static void main(String[] args) {
+        int studentAge = 13;
+        double studentGPA = 3.45;
+        String studentName = "Sima";
+        char studentInitial = studentName.charAt(0);
+        boolean studentIsMale = true;
+
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(studentInitial);
+        System.out.println(studentIsMale);
+        System.out.println(studentName);
+
+    }
+}
