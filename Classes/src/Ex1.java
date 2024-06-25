6722f8cc67e2ff7c69be105bd0235fb9d025c055
sima golrokh bahoush <sima.golrokh@gmail.com>
2024-06-25 10:51:22 +0200

    edit

--- Classes/src/Ex1.java
+++ Classes/src/Ex1.java
@@ -1,33 +1,38 @@
 public class Ex1 {
-        // Variable representing current balance
-        private int balance = 0;
-        int depositAmount ;
-        int withdrawAmount ;
+    // Variable representing current balance
+    private int balance ;
+    int depositAmount;
+    int withdrawAmount;
 
-        public Ex1(int depositAmount, int withdrawAmount) {
-            this.depositAmount = depositAmount;
-            this.withdrawAmount = withdrawAmount;
+    public Ex1(int depositAmount, int withdrawAmount) {
+        this.depositAmount = depositAmount;
+        this.withdrawAmount = withdrawAmount;
+        this.balance = depositAmount - withdrawAmount;
+    }
+
+    public void withdraw(int withdrawAmount) {
+        // Implement withdrawal logic
+        if (withdrawAmount <= balance) {
+            balance -= withdrawAmount;
         }
+    }
 
-        public void withdraw(int amount) {
-            // Implement withdrawal logic
-            if (amount <= balance) {
-                balance -= amount;
-            }
+    public void deposit(int depositAmount) {
+        // Implement deposit logic
+        if (depositAmount > 0) {
+            balance += depositAmount;
         }
+    }
 
-        public void deposit(int amount) {
-            // Implement deposit logic
-            if (amount > 0) {
-                balance += amount;
-            }
-        }
+    public int getBalance() {
+        // Implement logic to return current balance
+        return balance;
+    }
 
-        public int getBalance() {
-            // Implement logic to return current balance
-            return balance;
-        }
+    public static void main(String[] args) {
+        Ex1 ex1 = new Ex1(150, 100);
 
- public static void main(String[] args) {
-    Ex1 ex1 = new Ex1(100, 100);
-    System.out.println(ex1.getBalance());}
+        int balance = ex1.getBalance();
+        System.out.println(balance);
+    }
+}
